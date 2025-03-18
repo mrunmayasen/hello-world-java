ab0d509980be1ea4791e7dad97a8024aade53e69
root <root@ip-172-31-37-200.ap-south-1.compute.internal>
2025-03-18 10:26:37 +0000

    3rd commit

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
+# Use an official OpenJDK image as the base image
 FROM openjdk:17
 
 # Set the working directory inside the container
 WORKDIR /app
 
+# Copy the Java source file into the container
 COPY HelloWorld.java .
 
+# Compile the Java program
 RUN javac HelloWorld.java
 
+# Define the command to run the application
 CMD ["java", "HelloWorld"]
 
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,5 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
-@RequestMapping("/")
 public class HelloWorld {
-    @GetMapping
-    public String sayHello() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorld.class, args);
-    }
-}
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
