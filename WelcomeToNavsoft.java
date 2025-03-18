7ab51ab6d1dcbb569fe6491b530153c9a05ad543
root <root@ip-172-31-37-200.ap-south-1.compute.internal>
2025-03-18 11:21:44 +0000

    9th

--- WelcomeToNavsoft.java
+++ WelcomeToNavsoft.java
@@ -1,7 +1,7 @@
 public class WelcomeToNavsoft {
   public static void main(String[] args) {
     while (true) {
-      System.out.println("Hello world!");
+      System.out.println("WelcomeToNavsoft!");
       try {
         Thread.sleep(5000); // Wait 5 seconds before printing again
       } catch (InterruptedException e) {
